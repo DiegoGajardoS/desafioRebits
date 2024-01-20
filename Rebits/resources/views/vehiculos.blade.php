a6d4b0fec447d896b2a4db3e4b7019463279a766
DiegoGajardoS <diego.gajardo.s@usach.cl>
2024-01-20 04:22:41 -0300

    navbar

--- Rebits/resources/views/vehiculos.blade.php
+++ Rebits/resources/views/vehiculos.blade.php
@@ -4,15 +4,39 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
-	<title></title>
+  
+	<title>Vehiculos</title>
+
+  <style>
+        body {
+            background-color: #4aeadc;
+        }
+    </style>
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Usuarios</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
 <div class="p-5 table-responsive">
-  <h1 class="text-center p-3">VEHICULOS</h1>
+ 
   <table class="table table-striped table-bordered table-hover">
   <thead class="titulo-tabla">
+    
     <tr>
       <th scope="col">#ID</th>
       <th scope="col">Marca</th>
